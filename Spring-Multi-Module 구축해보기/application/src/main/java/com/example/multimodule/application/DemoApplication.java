54e1d753e7b0bccb044396fee2aa747dc2854145
zieunx <wldms_1217@naver.com>
2020-10-30 13:58:25 +0900

    실행 및 테스트 코드 추가

---
+++ Spring-Multi-Module 구축해보기/application/src/main/java/com/example/multimodule/application/DemoApplication.java
@@ -0,0 +1,47 @@
+package com.example.multimodule.application;
+
+import com.example.multimodule.service.MyService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/*
+(번역)
+@SpringBootApplication 다음을 모두 추가하는 편리한 주석입니다.
+
+@Configuration: 애플리케이션 컨텍스트에 대한 Bean 정의 소스로 클래스에 태그를 지정합니다.
+
+@EnableAutoConfiguration: Spring Boot에 클래스 경로 설정, 기타 Bean 및 다양한 속성 설정에 따라 Bean 추가를 시작하도록 지시합니다. 예를 spring-webmvc들어가 클래스 경로에있는 경우이 주석은 애플리케이션을 웹 애플리케이션으로 플래그 지정하고 DispatcherServlet.
+
+@ComponentScan: Spring에 com/example패키지 에서 다른 구성 요소, 구성 및 서비스를 찾도록 지시 하여 컨트롤러를 찾을 수 있도록합니다.
+* */
+
+@SpringBootApplication(scanBasePackages = "com.example.multimodule")
+@RestController
+public class DemoApplication {
+
+    private final MyService myService;
+
+
+    public DemoApplication(MyService myService) {
+        this.myService = myService;
+    }
+
+    @GetMapping("/")
+    public String name() {
+        return myService.message();
+    }
+
+    public static void main(String[] args) {
+        /*
+        (번역)
+        이 main()방법은 Spring Boot의 SpringApplication.run()방법을 사용하여 애플리케이션을 시작합니다.
+        XML이 한 줄도 없다는 것을 알고 계셨습니까?
+        web.xml파일 도 없습니다.
+        이 웹 애플리케이션은 100 % 순수 Java이며 배관이나 인프라를 구성 할 필요가 없습니다.
+        * */
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
--- Spring-Multi-Module 구축해보기/library/src/main/java/com/example/multimodule/service/MyService.java
+++ Spring-Multi-Module 구축해보기/library/src/main/java/com/example/multimodule/service/MyService.java
@@ -1,6 +1,6 @@
 package com.example.multimodule.service;
 
-import jdk.jfr.Enabled;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 
