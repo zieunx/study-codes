673625f5a21e859c89ae2e55a135cd5273277246
zieunx <wldms_1217@naver.com>
2021-09-16 18:14:48 +0900

    common 모듈 추가

--- Spring-Multi-Module 구축해보기/application/src/main/java/com/example/multimodule/application/DemoApplication.java
+++ Spring-Multi-Module 구축해보기/application/src/main/java/com/example/multimodule/application/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.multimodule.application;
 import com.example.multimodule.service.MyService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 * */
 
 @SpringBootApplication(scanBasePackages = "com.example.multimodule")
-@RestController
 public class DemoApplication {
 
     private final MyService myService;
---
+++ Spring-Multi-Module 구축해보기/library/src/main/java/com/example/multimodule/service/MemberServiceCustom.java
@@ -0,0 +1,16 @@
+package com.example.multimodule.service;
+
+import com.example.multimodule.domain.Member;
+import com.example.multimodule.domain.MemberRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberServiceCustom {
+
+    private final MemberRepository memberRepository;
+
+    public MemberServiceCustom(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+}
