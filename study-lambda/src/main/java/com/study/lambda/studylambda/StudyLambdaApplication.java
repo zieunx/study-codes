7dbd033314397e374075409fcea3e1b8c1caf13f
zieunx <wldms_1217@naver.com>
2022-01-13 21:14:15 +0900

    feat(lambda): 프로젝트 생성

---
+++ study-lambda/src/main/java/com/study/lambda/studylambda/StudyLambdaApplication.java
@@ -0,0 +1,13 @@
+package com.study.lambda.studylambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyLambdaApplication.class, args);
+	}
+
+}
