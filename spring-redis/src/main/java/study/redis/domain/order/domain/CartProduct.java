f039d67cfcfd2d45ead12a6532c7ff8021e79a9c
zieunx <wldms_1217@naver.com>
2022-11-02 23:51:16 +0900

    feat(redis/order): CartProduct 추가

---
+++ spring-redis/src/main/java/study/redis/domain/order/domain/CartProduct.java
@@ -0,0 +1,46 @@
+package study.redis.domain.order.domain;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(of = {"productCode"})
+public class CartProduct {
+    private String productCode;
+    private String name;
+    private Integer price;
+    private Integer count;
+
+    @Builder
+    public CartProduct(String productCode, String name, Integer price, Integer count) {
+        this.productCode = productCode;
+        this.name = name;
+        this.price = price;
+        this.count = count;
+    }
+
+    public void plusCount(int count) {
+        this.count += count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public boolean sameProduct(CartProduct addProduct) {
+        return addProduct.sameCode(this.productCode);
+    }
+
+    private boolean sameCode(String productCode) {
+        return this.productCode.equals(productCode);
+    }
+
+    @Override
+    public String toString() {
+        return "CartProduct{" +
+                "productCode='" + productCode + '\'' +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", count=" + count +
+                "}\n";
+    }
+}
