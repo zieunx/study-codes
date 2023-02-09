1b413b182b3cd62fefde13cbda0ccdc4f6a8d020
zieunx <wldms_1217@naver.com>
2023-02-09 17:29:42 +0900

    feat(spring-data-jpa): @Basic 실습

---
+++ spring-data-jpa/src/main/java/study/spring/jpa/model/Product.java
@@ -0,0 +1,38 @@
+package study.spring.jpa.model;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Basic(optional = false)
+    private String name;
+
+    private Integer price;
+
+    @Lob
+    private String description;
+
+    @Column(length = 16)
+    private UUID productCode;
+
+    public Product(String name, Integer price, String description, UUID productCode) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.productCode = productCode;
+    }
+}
---
+++ spring-data-jpa/src/main/java/study/spring/jpa/model/ProductRepository.java
@@ -0,0 +1,8 @@
+package study.spring.jpa.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
