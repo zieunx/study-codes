bb7a956b42bcc29ad5001b3348659d4d31b70072
zieunx <wldms_1217@naver.com>
2023-02-28 17:01:52 +0900

    feat(spring-data-jpa): 트리형태의 엔티티 '카테고리' 추가

--- spring-data-jpa/src/main/java/study/spring/jpa/category/model/CategoryRepository.java
+++ spring-data-jpa/src/main/java/study/spring/jpa/category/model/CategoryRepository.java
@@ -1,6 +1,19 @@
 package study.spring.jpa.category.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    @Query("SELECT category FROM Category category WHERE category.parentCategory.id = :id")
+    List<Category> findChildren(@Param(value = "id") Long id);
+
+    @Query("SELECT category FROM Category category WHERE category.parentCategory.id = :id AND category.displayYn = :displayYn")
+    List<Category> findAllChildren(@Param(value = "id") Long id,
+                                   @Param(value = "displayYn") boolean displayYn);
+
+    @Query("SELECT category FROM Category category where category.parentCategory.id is null AND category.displayYn = :displayYn")
+    List<Category> findRootCategories(@Param(value = "displayYn") boolean displayYn);
 }
