c4046b9def2e2b8453a2633cbd3158c6da47cfcd
zieunx <wldms_1217@naver.com>
2023-03-04 02:56:18 +0900

    feat(spring-data-jpa): Category displayYn 컬럼 추가

--- spring-data-jpa/src/main/java/study/spring/jpa/product/model/ProductRepository.java
+++ spring-data-jpa/src/main/java/study/spring/jpa/product/model/ProductRepository.java
@@ -3,6 +3,7 @@ package study.spring.jpa.product.model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.LockModeType;
@@ -13,5 +14,5 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT product FROM Product product WHERE product.id = :id")
-    Optional<Product> findByIdForUpdate(Long id);
+    Optional<Product> findByIdForUpdate(@Param("id") Long id);
 }
