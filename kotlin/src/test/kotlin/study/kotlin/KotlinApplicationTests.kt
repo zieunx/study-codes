b755153410abf23cf0fc17efb1f18c621e21a45e
zieunx2 <jieun@steppay.kr>
2023-03-14 16:42:36 +0900

    feat(kotlin): 코틀린 프로젝트 추가

---
+++ kotlin/src/test/kotlin/study/kotlin/KotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
