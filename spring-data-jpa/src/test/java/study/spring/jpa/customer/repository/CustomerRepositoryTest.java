02a28f11d7ffc014165225b8f302e17cfa6ecd75
zieunx <wldms_1217@naver.com>
2023-03-17 23:34:02 +0900

    feat(spring-data-jpa): CustomerRepositoryTest

---
+++ spring-data-jpa/src/test/java/study/spring/jpa/customer/repository/CustomerRepositoryTest.java
@@ -0,0 +1,11 @@
+package study.spring.jpa.customer.repository;
+
+import org.junit.jupiter.api.Test;
+
+public class CustomerRepositoryTest {
+
+    @Test
+    void 결제수단_삭제시_연결데이터가_있으면_에러발생() {
+
+    }
+}
