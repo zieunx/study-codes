6e58b02d1d3f11a9e208b1f4858afd4468f3de78
zieunx <wldms_1217@naver.com>
2023-03-28 01:09:43 +0900

    feat(kotlin/validation): nullable로 바꿔주면 validation 수행

--- kotlin/src/main/kotlin/study/kotlin/dto/ProductNotNullRequest.kt
+++ kotlin/src/main/kotlin/study/kotlin/dto/ProductNotNullRequest.kt
@@ -1,8 +1,8 @@
 package study.kotlin.dto
 
-import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
 
 data class ProductNotNullRequest (
-    @field:NotBlank(message = "상품명은 필수값입니다.")
-    val productName: String
+    @field:NotNull(message = "상품명은 필수값입니다.")
+    val productName: String?
 )
