5e4cad572196537d40b34e74384d2fb2b3653360
zieunx <wldms_1217@naver.com>
2023-04-11 00:11:43 +0900

    feat(kotlin/coroutine): http통신 비동기 처리 예제 미리 생성

--- kotlin/src/main/kotlin/study/kotlin/controller/ProductCreateController.kt
+++ kotlin/src/main/kotlin/study/kotlin/controller/ProductCreateController.kt
@@ -4,18 +4,25 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import study.kotlin.dto.ProductCreateRequest
 import study.kotlin.dto.ProductNotNullRequest
+import study.kotlin.service.ProductService
 import javax.validation.Valid
 
 @RestController
 @RequestMapping("/product")
-class ProductCreateController {
+class ProductCreateController(
+    private val productService: ProductService
+) {
 
     @PostMapping
     fun createProduct(
         @RequestBody @Valid productCreateRequest: ProductNotNullRequest
     ) {
         println("상품생성 요청 데이터: $productCreateRequest")
+    }
+
+    @PostMapping("/connect")
+    suspend fun connectMultiApi() {
+        productService.callApi()
     }
 }
