22b351f352a53de33cc7ed36ddf09c9967aa5614
zieunx <wldms_1217@naver.com>
2023-04-11 21:54:47 +0900

    feat(kotlin/coroutine): 비동기로 작동하도록 수정

--- kotlin/src/main/kotlin/study/kotlin/service/ProductClient.kt
+++ kotlin/src/main/kotlin/study/kotlin/service/ProductClient.kt
@@ -1,22 +1,29 @@
 package study.kotlin.service
 
 import io.ktor.client.*
+import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
 import org.springframework.stereotype.Service
-import java.lang.Thread.sleep
 
 @Service
-class ProductClient(
-    private val httpClient: HttpClient
-) {
+class ProductClient {
 
-    suspend fun call() {
-        sleep(1000)
-        httpClient.post("https://www.naver.com/")
+    private val httpClient = HttpClient(CIO)
+
+    suspend fun call() = coroutineScope {
+        async {
+            delay(1000)
+            httpClient.post<Unit>("https://www.naver.com/")
+        }
     }
 
-    suspend fun call2() {
-        sleep(2000)
-        httpClient.post("https://www.naver.com/")
+    suspend fun call2() = coroutineScope {
+        async {
+            delay(2000)
+            httpClient.post<Unit>("https://www.naver.com/")
+        }
     }
 }
