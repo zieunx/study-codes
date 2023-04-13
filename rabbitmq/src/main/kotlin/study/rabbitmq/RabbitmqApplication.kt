6f0873a4bfc237c9d7530b9b391426078bb0f7c0
zieunx <wldms_1217@naver.com>
2023-04-13 23:53:24 +0900

    feat(rabbitmq): project init

---
+++ rabbitmq/src/main/kotlin/study/rabbitmq/RabbitmqApplication.kt
@@ -0,0 +1,11 @@
+package study.rabbitmq
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RabbitmqApplication
+
+fun main(args: Array<String>) {
+	runApplication<RabbitmqApplication>(*args)
+}
---
+++ rabbitmq/src/test/kotlin/study/rabbitmq/RabbitmqApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.rabbitmq
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RabbitmqApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
