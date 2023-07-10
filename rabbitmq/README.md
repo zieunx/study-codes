27e74be5e7b48a0dca9ee71f5e2b55352308c388
zieunx <wldms_1217@naver.com>
2023-07-11 00:50:39 +0900

    feat(rabbitmq): dlq ttl

--- rabbitmq/README.md
+++ rabbitmq/README.md
@@ -23,3 +23,13 @@ TCP 소캣을 통해 물리적 연결
 - 채널을 캐싱해서 리소스 최적화
   - 하지만 thread-safe 하지는 않음
 
+## DLQ
+
+일정 시간이 지나면 메시지를 처리할 수 있도록 설계할 수 있다.
+
+- `x-dead-letter-exchange` : DLQ 로 메시지를 보낼 exchange
+- `x-dead-letter-routing-key` : DLQ 로 메시지를 보낼 routing key
+- `x-message-ttl` : 메시지의 TTL (Time To Live)
+
+이 설정으로 Queue를 등록하면, 메시지는 최대 10초 동안 큐에 머무를 수 있다.  
+10초가 지나면 메시지는 처리되지 않은 상태로 큐에서 제거되고, 설정된 Dead Letter Exchange를 통해 Dead Letter Queue로 전달된다.
--- rabbitmq/src/main/kotlin/study/rabbitmq/domain/producer/config/RabbitMqConfig.kt
+++ rabbitmq/src/main/kotlin/study/rabbitmq/domain/producer/config/RabbitMqConfig.kt
@@ -15,7 +15,7 @@ import javax.annotation.PostConstruct
 
 @Configuration
 class RabbitMqConfig(
-    private val amqpAdmin: AmqpAdmin
+    private val amqpAdmin: AmqpAdmin,
 ) {
 
     @PostConstruct
@@ -26,12 +26,12 @@ class RabbitMqConfig(
 
         QueueBuilder.durable(RabbitMqQueue.TEST_QUEUE)
             .build()
-            .let {queue ->
+            .let { queue ->
                 amqpAdmin.declareQueue(queue)
                 amqpAdmin.declareBinding(
                     BindingBuilder.bind(queue)
                         .to(exchange)
-                        .with(RabbitMqRouteKey.TEST_ROUTE_KEY)
+                        .with(RabbitMqRouteKey.TEST_ROUTE_KEY),
                 )
             }
     }
@@ -42,11 +42,20 @@ class RabbitMqConfig(
     }
 
     @Bean
-    fun ttlQueue(): Queue {
+    fun ttl5Queue(): Queue {
+        return ttlQueue("ttl5Queue", "myExchange", 5000)
+    }
+
+    fun ttlQueue(
+        queueName: String, // 해당 ttl Queue 이름
+        exchangeName: String,
+        ttl: Int,
+    ): Queue {
         val arguments = mapOf(
-            "x-dead-letter-exchange" to "x2",
-            "x-message-ttl" to 5000,
+            "x-dead-letter-exchange" to exchangeName, // 전송 될 Exchange
+            "x-message-ttl" to ttl, // 지연 시간
+            // "x-dead-letter-routing-key" to "지연된 메시지가 전송 될 라우팅 키"
         )
-        return Queue("ttlQueue", false, false, false, arguments)
+        return Queue(queueName, false, false, false, arguments)
     }
 }
